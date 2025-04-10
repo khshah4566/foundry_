8c020789f680c5c0e5e8c0b5b890f8682ad36892
khshah4566 <khshah@my.bridgeport.edu>
2025-04-10 14:20:38 -0400

    updated solidity

--- script/UpdateBook.s.sol
+++ script/UpdateBook.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity 0.8.9;
 
 import "../src/Books.sol"; // Path to your Books contract
 import "forge-std/Script.sol"; // Import Foundry Script functionality
